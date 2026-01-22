84a43629cafc342601eca0a43a63e3c22241c5c8
Zbalah Addem <addem.zbalah@etu.unilim.fr>
2026-01-22 14:58:21 +0100

    Structure terminé

--- js/fishing-rod.js
+++
@@ -1,6 +0,0 @@
-AFRAME.registerComponent('fishing-rod', {
-  init: function () {
-    this.el.setAttribute('geometry', { primitive: 'cylinder', radius: 0.02, height: 1 });
-    this.el.setAttribute('material', { color: '#8B4513' });
-  }
-});
--- js/main.js
+++
@@ -1,7 +0,0 @@
-AFRAME.registerComponent('init-game', {
-  init: function () {
-    console.log("Jeu de pêche XR initialisé !");
-  }
-});
-
-document.querySelector('a-scene').setAttribute('init-game', '');
